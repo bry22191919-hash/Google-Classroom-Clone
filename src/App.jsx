4a8b4b5f2de4e27e717152f716bf7792e6b67c18
unknown <bry22191919@gmail.com>
2025-11-10 21:10:51 +0800

    Added Log In and Sign Up in file system.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react'
+import {HashRouter as Router, Routes, Route} from 'react-router-dom'
 import './App.css'
+import LogIn from './Pages/LogIn.jsx'
+import SignUp from './Pages/SignUp.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-     <div>
-      <p>Hello, world!</p>
-     </div>
+   <Router>
+    <Routes>
+      <Route path="/" element={<LogIn/>}/>
+      <Route path="/login" element={<LogIn/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+    </Routes>
+   </Router>
   )
 }
 
-export default App
+export default App;
